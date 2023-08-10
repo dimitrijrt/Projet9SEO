f76ca9b9f9a917f3328b29c1cc86f5c441f8ba89
dimitrijrt <dimitrijacquart@gmail.com>
2023-08-10 17:47:13 +0200

    img idth

--- assets/maugallery.js
+++ assets/maugallery.js
@@ -18,7 +18,7 @@ let index=0,next=null;$(imagesCollection).each(function(i){if($(activeImage).att
                                 ? '<div class="mg-prev" style="cursor:pointer;position:absolute;top:50%;left:-15px;background:white;"><</div>'
                                 : '<span style="display:none;" />'
                             }
-                            <img class="lightboxImage img-fluid" alt="Contenu de l'image affichée dans la modale au clique"/>
+                            <img class="fluidlightboxImage img-" alt="Contenu de l'image affichée dans la modale au clique"/>
                             ${
                               navigation
                                 ? '<div class="mg-next" style="cursor:pointer;position:absolute;top:50%;right:-15px;background:white;}">></div>'
